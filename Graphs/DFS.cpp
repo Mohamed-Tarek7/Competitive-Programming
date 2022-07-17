0ad167c2a6dbf8658ff16f9ab5e7c9b49294bde7
Mohamed Tarek <100866513+Mohamed-Tarek7@users.noreply.github.com>
2022-07-17 15:19:50 +0200

    Create DFS.cpp

---
+++ Graphs/DFS.cpp
@@ -0,0 +1,12 @@
+const int N = 1e5 + 1;
+
+vt<int> adj[N];
+bool vis[N];
+
+void dfs(int node){
+  vis[node] = 1;
+  FOR(i, 0, sz(adj[node])){
+    int child = adj[node][i];
+    if (!vis[child]) dfs(child);
+  }
+}
