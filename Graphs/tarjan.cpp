99bbd738b50b5b7a16d4269935ab70ce1e4a1159
Mohamed Tarek <100866513+Mohamed-Tarek7@users.noreply.github.com>
2022-07-17 16:33:02 +0200

    Create tarjan.cpp

---
+++ Graphs/tarjan.cpp
@@ -0,0 +1,31 @@
+const int N = 1e5 + 1;
+
+vt<int> adj[N], lowlink(N), dfn(N, -1);
+vt<vt<int>> comps;
+int ndf = 0;
+bool instack[N];
+stack<int> st;
+
+void tarjan(int node){
+	lowlink[node] = dfn[node] = ndf++;
+	instack[node] = 1;
+	st.push(node);
+	FOR(i, 0, sz(adj[node])){
+		int child = adj[node][i];
+		if (dfn[child] == -1){
+			tarjan(child);
+			lowlink[node] = min(lowlink[node], lowlink[child]);
+		}
+		else if (instack[child]) lowlink[node] = min(lowlink[node], dfn[child]);
+	}
+	if (lowlink[node] == dfn[node]){
+		comps.pb(vt<int> ());
+		int x = -1;
+		while (x != node){
+			x = st.top();
+			st.pop();
+			instack[x] = 0;
+			comps.back().pb(x);
+		}
+	}
+}
