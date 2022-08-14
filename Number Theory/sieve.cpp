e3c6938b666260d425611b15b21106ce8894e66c
Mohamed Tarek <100866513+Mohamed-Tarek7@users.noreply.github.com>
2022-08-14 20:35:06 +0200

    Create sieve.cpp

---
+++ Number Theory/sieve.cpp
@@ -0,0 +1,10 @@
+bool prime[N];
+
+void sieve(){
+	prime[0] = prime[1] = true;
+	for (int i = 2 ; i <= N ; i++){
+		if (!prime[i]){
+			for (int j = 2 * i ; j <= N ; j += i) prime[j] = true;
+		}
+	}
+}
