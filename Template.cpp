8693345e144a298ca1e9f71c661f18215b65f8aa
Mohamed Tarek <100866513+Mohamed-Tarek7@users.noreply.github.com>
2022-08-14 21:14:31 +0200

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std::chrono;
+using namespace __gnu_pbds;
+
+#define FAST() ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define read(x) for (auto &it : (x)) cin >> it;
+#define readpair(x) for (auto &it : (x)) cin >> it.first >> it.second;
+#define print(x) for (auto &it : (x)) cout << it << " ";
+#define printpair(x) for (auto &it : (x)) cout << it.first << " " << it.second << '\n';
+#define FOR(i, a, b) for (int i = a ; i < b ; i++)
+#define FORe(i, a, b) for (int i = a ; i <= b ; i++)
+#define FORd(i, a, b) for (int i = a ; i >= b ; i--)
+#define FORp(i, a, b, x) for (int i = a ; i <= b ; i += x)
+#define FORdp(i, a, b, x) for (int i = a ; i >= b ; i -= x)
+#define EACH(i, x) for (auto &i : (x))
+#define Fill(x, n) memset(x, n, sizeof x)
+#define sz(x) int(x.size())
+#define READ(FILE) freopen(FILE, "r", stdin);
+#define WRITE(FILE) freopen(FILE, "w", stdout);
+#define ll long long
+#define ull unsigned long long 
+#define vt vector
+#define pq priority_queue
+#define pii pair<int, int> 
+#define MP make_pair
+#define pb push_back
+#define f first
+#define s second
+#define tolower(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
+#define toupper(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
+
+const double pi = acos(-1);
+const int N = 1e6 + 1, mod = 1e9 + 7;
+const ll OO = LONG_LONG_MAX;
+const int oo = 0x7f7f7f7f;
+int dx4[] = {-1, 0, 0, 1};
+int dy4[] = {0, -1, 1, 0};
+int dx8[] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy8[] = {1, -1, 0, 0, 1, -1, -1, 1};
+
+void solve(){
+	
+}
+
+int main(){
+	//READ("FILE");	
+    FAST();
+    int t = 1;
+	//cin >> t;
+	//preprocess();
+    while (t--){
+		//init();
+		solve();
+	}
+	return 0;
+}
