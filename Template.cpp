496c1bb784758484809b863167f52430f66c0256
Mohamed Tarek <100866513+Mohamed-Tarek7@users.noreply.github.com>
2022-09-08 09:42:30 +0200

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -11,23 +11,23 @@ using namespace __gnu_pbds;
 #define allr(x) x.rbegin(), x.rend()
 #define read(x) for (auto &it : (x)) cin >> it;
 #define readpair(x) for (auto &it : (x)) cin >> it.first >> it.second;
+#define read2D(x, n, m) for (int i = 0; i < n; i++) for (int j = 0; j < m && cin >> x[i][j]; j++);
+#define print2D(x, n, m) for (int i = 0; i < n; i++, cout << '\n') for (int j = 0; j < m && cout << x[i][j] << " "; j++);
 #define print(x) for (auto &it : (x)) cout << it << " ";
 #define printpair(x) for (auto &it : (x)) cout << it.first << " " << it.second << '\n';
-#define FOR(i, a, b) for (int i = a ; i < b ; i++)
-#define FORe(i, a, b) for (int i = a ; i <= b ; i++)
-#define FORd(i, a, b) for (int i = a ; i >= b ; i--)
-#define FORp(i, a, b, x) for (int i = a ; i <= b ; i += x)
-#define FORdp(i, a, b, x) for (int i = a ; i >= b ; i -= x)
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define FORe(i, a, b) for (int i = a; i <= b; i++)
+#define FORd(i, a, b) for (int i = a; i >= b; i--)
+#define FORp(i, a, b, x) for (int i = a; i <= b; i += x)
+#define FORdp(i, a, b, x) for (int i = a; i >= b; i -= x)
 #define EACH(i, x) for (auto &i : (x))
 #define Fill(x, n) memset(x, n, sizeof x)
 #define sz(x) int(x.size())
 #define READ(FILE) freopen(FILE, "r", stdin);
 #define WRITE(FILE) freopen(FILE, "w", stdout);
 #define ll long long
-#define ull unsigned long long 
-#define vt vector
+#define ull unsigned long long
 #define pq priority_queue
-#define pii pair<int, int> 
 #define MP make_pair
 #define pb push_back
 #define f first
@@ -37,7 +37,7 @@ using namespace __gnu_pbds;
 #define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
 
 const double pi = acos(-1);
-const int N = 1e6 + 1, mod = 1e9 + 7;
+const int N = 1e5 + 1, mod = 1e9 + 7;
 const ll OO = LONG_LONG_MAX;
 const int oo = 0x7f7f7f7f;
 int dx4[] = {-1, 0, 0, 1};
@@ -46,18 +46,18 @@ int dx8[] = {0, 0, 1, -1, 1, -1, 1, -1};
 int dy8[] = {1, -1, 0, 0, 1, -1, -1, 1};
 
 void solve(){
-	
+    
 }
 
 int main(){
-	//READ("FILE");	
+    //READ("FILE");
     FAST();
     int t = 1;
-	//cin >> t;
-	//preprocess();
+    //cin >> t;
+    //preprocess();
     while (t--){
-		//init();
-		solve();
-	}
-	return 0;
+        //init();
+        solve();
+    }
+    return 0;
 }
