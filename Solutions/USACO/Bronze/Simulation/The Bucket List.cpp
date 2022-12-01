c79735f383889f907666416a297699bef847057c
Mohamed Tarek <100866513+Mohamed-Tarek7@users.noreply.github.com>
2022-12-01 13:16:07 +0200

    Update The Bucket List.cpp

--- Solutions/USACO/Bronze/Simulation/The Bucket List.cpp
+++ Solutions/USACO/Bronze/Simulation/The Bucket List.cpp
@@ -7,31 +7,31 @@ using namespace std;
 vector<pair<int, ll>> S(1001), F(1001);
 
 void solve(){
-	int n; cin >> n;
-	int mx = 0;
-	for (int i = 0; i < n; i++){
-		int s, t, b; cin >> s >> t >> b;
-		++S[s].first;
-		S[s].second += b;
-		++F[t].first;
-		F[t].second += b;
-		mx = max(mx, t);
+   int n; cin >> n;
+   int mx = 0;
+   for (int i = 0; i < n; i++){
+	int s, t, b; cin >> s >> t >> b;
+	++S[s].first;
+	S[s].second += b;
+	++F[t].first;
+	F[t].second += b;
+	mx = max(mx, t);
+   }
+   int ans = 0, cnt = 0;
+   ll B = 0;
+   for (int i = 1; i <= mx; i++){
+	if (S[i].first){
+	   B += S[i].second;
+	   cnt += S[i].first;
 	}
-	int ans = 0, cnt = 0;
-	ll B = 0;
-	for (int i = 1; i <= mx; i++){
-		if (S[i].first){
-			B += S[i].second;
-			cnt += S[i].first;
-		}
-		if (F[i].first){
-			B -= F[i].second;
-			cnt -= F[i].first;
-		}
-		if (cnt > 1)
-			ans = max(1ll * ans, B);
+	if (F[i].first){
+	   B -= F[i].second;	
+	   cnt -= F[i].first;
 	}
-	cout << ans;
+	if (cnt > 1)
+	   ans = max(1ll * ans, B);
+   }
+   cout << ans;
 }
 
 int main(){
