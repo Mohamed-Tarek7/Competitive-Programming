076aece1b1201d3cfe2309abcbeb35f5eedd405f
Mohamed Tarek <100866513+Mohamed-Tarek7@users.noreply.github.com>
2022-12-01 13:09:40 +0200

    Create The Bovine Shuffle.cpp

---
+++ Solutions/USACO/Bronze/Simulation/The Bovine Shuffle.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST() ios::sync_with_stdio(0), cin.tie(0), cout.tie(0
+#define ll long long
+
+void solve(){
+	int n; cin >> n;
+	vector<int> A(n);
+	vector<string> B(n);
+	for (int i = 0; i < n; i++)
+		cin >> A[i];
+	for (int i = 0; i < n; i++)
+		cin >> B[i];
+	for (int i = 1; i <= 3; i++){
+		auto C = B;
+		for (int j = 0; j < n; j++){
+			C[j] = B[A[j] - 1]; 
+		}
+		B = C;
+	}
+	for (int i = 0; i < n; i++)
+		cout << B[i] << '\n';
+}
+
+int main(){
+    freopen("shuffle.in", "r", stdin);
+    freopen("shuffle.out", "w", stdout);
+    FAST();
+    int tc = 1;
+    //cin >> tc;
+    while (tc--)
+        solve();
+    return 0;
+}
