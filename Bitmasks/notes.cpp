29e8b374f83ad0651979456f6f46f4227e07a6a2
Mohamed Tarek <100866513+Mohamed-Tarek7@users.noreply.github.com>
2024-12-11 18:36:04 +0200

    Update notes.cpp

--- Bitmasks/notes.cpp
+++ Bitmasks/notes.cpp
@@ -2,3 +2,4 @@ mask = mask | (1 << bit) // Set a bit to one
 mask = mask & ~(1 << bit) // Set a bit to zero
 mask = mask ^ (1 << bit) // Flip a bit
 x ^ y ^ x = y
+A number is divisible by 9 if the sum on its digits is divisible by 9
